889a1c76c3503dea31d10ac5e37c49263626ecb3
Stni18 <snferrer21@gmail.com>
2021-06-15 19:28:19 +0000

    Add formEntity in contact page. View form content in Datastore.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -7,6 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+
+
 /** Servlet that processes text. */
 @WebServlet("/form-handler")
 public final class FormHandlerServlet extends HttpServlet {
@@ -20,18 +27,39 @@ public final class FormHandlerServlet extends HttpServlet {
         String subjectValue = request.getParameter("subject");
         String commentValue = request.getParameter("text-input");
 
+        //new variables
+        long timestamp = System.currentTimeMillis();
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Form");
+
+
+    FullEntity formEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("name", nameValue)
+            .set("email", emailValue)
+            .set("subject", subjectValue)
+            .set("text-input", commentValue)
+            .set("timestamp", timestamp)
+            .build();
+            datastore.put(formEntity);
+
+
+
         // Print the value so you can see it in the server logs.
         System.out.println("Name: " + nameValue);
         System.out.println("Email: " + emailValue);
         System.out.println("subject: " + subjectValue);
         System.out.println("Comments: " + commentValue);
 
+        response.sendRedirect("/contact.html");
+
+        /*
         // Write the value to the response so the user can see it.
         response.getWriter().println("Name: " + nameValue);
         response.getWriter().println("Email: " + emailValue);
         response.getWriter().println("Subject: " + subjectValue);
         response.getWriter().println("Comments: " + commentValue);
-
+        */
     }
 
 }
