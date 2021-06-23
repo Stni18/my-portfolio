f9f1d8897b9ccca06c9a4fd810e300ab438535f7
Stni18 <snferrer21@gmail.com>
2021-06-23 21:41:48 +0000

    Add GitHub/Linkedin logo in index page. Change pie chart to bar chart.

--- portfolio/src/main/webapp/formEmailNotice.js
+++ portfolio/src/main/webapp/formEmailNotice.js
@@ -1,10 +1,9 @@
 
 //shows a error message if form has no email and prevents form from being sent
 
- addEventListener("click", function(event) {
+ addEventListener("click", function() {
     var email = document.forms["form1"]["email"].value;
     if (email === null || email === "") {
         document.getElementById('error').innerHTML = "*Mandatory*";
-        event.preventDefault();
     }
 });
