7beda988c99eb8e35c41afeee7236f94f5ab8205
Stni18 <snferrer21@gmail.com>
2021-06-25 17:52:18 +0000

    Modify EventListener. Spacing with CSS. Format Documents.

--- portfolio/src/main/webapp/formEmailNotice.js
+++ portfolio/src/main/webapp/formEmailNotice.js
@@ -1,9 +1,10 @@
 
 //shows a error message if form has no email and prevents form from being sent
 
- addEventListener("click", function() {
+ addEventListener("submit", function(event) {
     var email = document.forms["form1"]["email"].value;
     if (email === null || email === "") {
         document.getElementById('error').innerHTML = "*Mandatory*";
+        event.preventDefault();
     }
 });
